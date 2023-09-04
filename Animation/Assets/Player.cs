247e483081cb0ed86424b481f9f6c2992847e1f5
Shakir Tempelman <shakir33@hotmail.co.uk>
2023-09-04 03:29:54 -0400

    Added project files, build, and updated readme

---
+++ Animation/Assets/Player.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    private float inputX;
+    private float groundRadius;
+    private float jumpForce;
+    private int movementSpeedX;
+    private Vector2 movement;
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
+    private bool isFacingRight;
+    private bool isPlayerTouchingGround;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private Transform groundLocation;
+
+    // Use this for initialization
+    void Start()
+    {
+        movementSpeedX = 5;
+        groundRadius = 0.2f;
+        jumpForce = 400f;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        isFacingRight = true;
+        isPlayerTouchingGround = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+
+        if(isPlayerTouchingGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myAnimator.SetBool("Grounded", false);
+            myRigidBody.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        isPlayerTouchingGround = Physics2D.OverlapCircle(groundLocation.position, groundRadius, whatIsGround);
+
+        myAnimator.SetBool("Grounded", isPlayerTouchingGround);
+        myAnimator.SetFloat("VerticalSpeed", myRigidBody.velocity.y);
+
+        inputX = Input.GetAxis("Horizontal");
+        movement = new Vector2(movementSpeedX * inputX, myRigidBody.velocity.y);
+        myRigidBody.velocity = movement;
+
+        myAnimator.SetFloat("HorizontalMovement", Mathf.Abs(inputX));
+
+        if (inputX > 0 && isFacingRight == false)
+            FlipSanta();
+        else if (inputX < 0 && isFacingRight == true)
+            FlipSanta();
+    }
+
+    void FlipSanta()
+    {
+        isFacingRight = !isFacingRight;
+        Vector2 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# GameProgramming-AnimLesson
- Game Programming Animation Lesson
+# Game Programming Animation Lesson
+This project was an introductory lesson on animation. Learning about animation controllers and animation clips was very eye opening on how video games use them.
+
+To run the game, open the build folder and run the Animation.exe file. However, to exit the build of the game, you will have to close the program manually which you can do from either the taskbar or task manager.
+
+To view the project in the Unity editor, open the Animation folder using UnityHub to open it as a project.
